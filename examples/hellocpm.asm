5213ac313eabb7c1062f4cfd494caf0efe04c56b
giomba <giomba@glgprograms.it>
2024-12-09 22:42:18 +0100

    Add CP/M hello world.

---
+++ examples/hellocpm.asm
@@ -0,0 +1,25 @@
+    org 0x100
+
+    ; Print the alphabet under CP/M
+PUBLIC _main
+_main:
+    ; increment letter
+    ld      hl,letter
+    ld      e,(hl)
+    inc     e
+    ld      (hl),e
+
+    ; system call "2" - print char
+    ld      c,$2
+    call    $5
+
+    ; if letter is 'Z', then end
+    ld      a,$5a
+    cp      e
+    jr      z,end
+
+    jp      _main
+
+end:
+    ; Return to CP/M
+    ret
